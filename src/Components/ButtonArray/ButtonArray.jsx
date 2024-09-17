052f538b8f91cf16033158cff048e998054c8d9f
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-17 14:46:11 -0700

    Built basic button functionality with selection limits

--- src/Components/ButtonArray/ButtonArray.jsx
+++ src/Components/ButtonArray/ButtonArray.jsx
@@ -1,10 +1,25 @@
 import './ButtonArray.scss';
 import {useState, useEffect} from 'react'
+import Icon from '../../assets/Icons/right-chevron.png'
 
 
 function ButtonArray({skillsList}) {
     console.log({skillsList})
     const [skillCategories, setSkillCategories] = useState([]);
+    const [selectedButtons, setSelectedButtons] = useState([]);
+        
+        const handleButtonClick = (skill) => {
+            console.log(skill);
+            if (selectedButtons.includes(skill)) {
+                setSelectedButtons(selectedButtons.filter((selected) => selected !== skill));
+                console.log(selectedButtons);
+            } else {
+                if (selectedButtons.length < 6){
+                    setSelectedButtons([...selectedButtons, skill])
+                    console.log(selectedButtons);
+                }
+            }
+        }
     
     useEffect(() => {
         // Using Set to remove duplicates
@@ -15,14 +30,21 @@ function ButtonArray({skillsList}) {
 
   return (
     <div>
-        {skillCategories.map((skillCat) => (
+        {skillCategories.map((skillCat, index) => (
         <article className="category-container">
-            <div>
-                <h3>{skillCat}</h3>
+            <div className="category__title">
+                <img className="icon" src={Icon}/><h3>{skillCat}</h3>
             </div>
-            <div>
+            <div className="category__item">
             {skillsList.filter((skills) => skills.category === skillCat).map((skills) => (
-                <span>{skills.skill}</span>
+                <button 
+                    key={skills.id}
+                    onClick={() => handleButtonClick(skills.id)}
+                    className={`btn buttons--${index} ${selectedButtons.includes(skills.id) ? 'selected' : ''}`}
+                    disabled={!selectedButtons.includes(skills.id) && selectedButtons.length >= 5}
+                    >
+                        {skills.skill}
+                    </button>
             ))}
 
             </div>
