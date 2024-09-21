1dc777535b46eb65d431e5bed37a0c3b677a6bda
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-21 11:55:00 -0700

    changed styling on results page to be better read

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './App.scss'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer/Footer.jsx';
 import LandingPage from './Pages/LandingPage/LandingPage.jsx';
 import QuizPage from './Pages/QuizPage/QuizPage.jsx';
 import Debugger from './Pages/Debugger/Debugger.jsx';
 import CodeChallenge from './Pages/CodeChallenge/CodeChallenge.jsx';
 import Results from './Pages/Results/Results.jsx';
+import NightMode from './Components/NightMode/NightMode.jsx'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] =useState()
 
   return (
       <BrowserRouter>
         <Routes>
+        {/* <NightMode handleChange={handleChange}/> */}
           <Route path="/" element={<LandingPage />}/>
           <Route path="/quiz" element={<QuizPage />}/>
           <Route path="/debugger" element={<Debugger />}/>
